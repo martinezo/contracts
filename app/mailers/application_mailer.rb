02bd2170367afcaebf29fc262b183ff891609f83
cesar-alamilla <czar.5@hotmail.com>
2015-09-03 12:12:10 -0500

    onfiguration_file_for_mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,11 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "genomma@live.com"
   #layout 'mailer'
-  
 	  def send_mail(email)
+	  	email_technical_secretary = "#{APP_CONFIG[:technical_secretary][:e_mail]}"
 		#@user = user
 		#@url  = 'http://codeHero.co'
-		mail(to: email, subject: 'Aprende a programar con nuestros cursos gratis')
+		mail(to: [email, email_technical_secretary], subject: 'Correo de prueba para nuestro mailer')
 	  end
 
 end
