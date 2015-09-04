a1e80f512b3af083f02c98050766c33d975285fb
cezar alejandro ramirez <genomma@live.com>
2015-09-04 03:41:54 -0500

    entity_created_renewal

--- db/migrate/20150904061353_create_system_renewals.rb
+++ db/migrate/20150904061353_create_system_renewals.rb
@@ -7,10 +7,10 @@ class CreateSystemRenewals < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+	add_foreign_key(:system_contracts, :catalogs_suppliers, column: 'supplier_id')
+	add_foreign_key(:system_contracts, :catalogs_devices, column: 'device_id')
+  	add_foreign_key(:catalogs_devices, :catalogs_locations, column: 'location_id')
+	add_foreign_key(:system_renewals, :system_contracts, column: 'contract_id')
+	add_foreign_key(:system_siteviews, :system_renewals, column: 'renewal_id')
   end
-	#add_foreign_key(:system_contracts, :catalogs_suppliers, column: 'supplier_id')
-	#add_foreign_key(:system_contracts, :catalogs_devices, column: 'device_id')
-  	#add_foreign_key(:catalogs_devices, :catalogs_locations, column: 'location_id')
-	#add_foreign_key(:system_renewals, :system_contracts, column: 'contract_id')
-	#add_foreign_key(:system_siteviews, :system_renewals, colum: 'renewal_id')
 end
--- db/schema.rb
+++ db/schema.rb
@@ -92,4 +92,6 @@ ActiveRecord::Schema.define(version: 20150904061353) do
   add_foreign_key "catalogs_devices", "catalogs_locations", column: "location_id"
   add_foreign_key "system_contracts", "catalogs_devices", column: "device_id"
   add_foreign_key "system_contracts", "catalogs_suppliers", column: "supplier_id"
+  add_foreign_key "system_renewals", "system_contracts", column: "contract_id"
+  add_foreign_key "system_siteviews", "system_renewals", column: "renewal_id"
 end
