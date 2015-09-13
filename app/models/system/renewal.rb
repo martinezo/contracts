c30e4bd2adcfd913b801b7720f6f7291f15db315
Daniela Cerrillo <nanish_20@hotmail.com>
2015-09-13 01:51:32 -0500

    Modificacion en vistas index, show.

--- app/models/system/renewal.rb
+++ app/models/system/renewal.rb
@@ -1,5 +1,5 @@
 class System::Renewal < ActiveRecord::Base
 has_many :Siteviews
-belongs_to :contract, :class_name => 'System::Contracts', :foreign_key => 'contract_id'
+belongs_to :contract, :class_name => 'System::Contract', :foreign_key => 'contract_id'
 validates :contract_id, :start_date, :end_date, presence: true
 end
