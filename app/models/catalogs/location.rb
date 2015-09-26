180ed62c97b930c8f7df5228aae69edcb43d9c1f
Daniela Cerrillo <nanish_20@hotmail.com>
2015-09-26 15:10:52 -0500

    Correcciones campos, traducciones

--- app/models/catalogs/location.rb
+++ app/models/catalogs/location.rb
@@ -2,6 +2,7 @@ class Catalogs::Location < ActiveRecord::Base
 has_many :Devices
 validates :department, :responsible, :email, presence: true
 
-def short_name(length=18)
+  def short_name(length=18)
 	department.truncate(length)
+  end
 end
