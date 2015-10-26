1b62e2c3d0b41775780db95b81290ec139568f84
Daniela Cerrillo <nanish_20@hotmail.com>
2015-10-25 18:25:24 -0600

    Views_reporter

--- app/pdfs/report_pdf.rb
+++ app/pdfs/report_pdf.rb
@@ -19,18 +19,19 @@ class ReportPDF < Prawn::Document
 	def line_items
 		move_down 20
 		font "Helvetica", :size => 10		
-		table line_item_rows, :width => 750, :column_widths => [180, 180, 180, 105, 105], :row_colors => ["8CB9DC" , "F0F0F0"], :header => true  do
+		table line_item_rows, :width => 750, :column_widths => [170, 170, 170, 50, 95, 95], :row_colors => ["8CB9DC" , "F0F0F0"], :header => true  do
 			row(0).font_style = :bold
-			columns(0...4).align = :left
+			columns(0...5).align = :left
+			#columns(3).align = :right
 		end
 	end
 
 	def line_item_rows
 		#[[I18n.t('pdf.device_id'),I18n.t('pdf.supplier_id'), I18n.t('pdf.contract_no'), I18n.t('pdf.start_date'), I18n.t('pdf.end_date')]] +
-		[[ "Nombre de Equipo" , "Proveedor" , "No. de Contrato" , "Fecha inicial" , "Fecha final" ]] +
+		[[ "Nombre de Equipo" , "Proveedor" , "No. de Contrato" , "Monto" , "Fecha inicial" , "Fecha final" ]] +
 		@report.map do |rep|
 		#@report.line_items.map do |item|
-			[rep.contract.device.name, rep.contract.supplier.business_name, rep.contract.contract_no, rep.start_date, rep.end_date]
+			[rep.contract.device.name, rep.contract.supplier.business_name, rep.contract.contract_no, rep.monto, rep.start_date, rep.end_date]
 		end
 	end
 	#end
