c462477c2de813a7534415703984d1fa509a8fec
Daniela Cerrillo <nanish_20@hotmail.com>
2015-12-03 10:48:46 -0600

    Supplier_view

--- app/models/catalogs/supplier.rb
+++ app/models/catalogs/supplier.rb
@@ -3,7 +3,7 @@ include Searchable
 
 has_many :Contracts
 validates :business_name, uniqueness: true, presence: true
-validates :contact, :phone, :email, presence: true
+validates :contact, :phone, :email, :phone_office, presence: true
 
 def short_name(length=18)
 	business_name.truncate(length)
