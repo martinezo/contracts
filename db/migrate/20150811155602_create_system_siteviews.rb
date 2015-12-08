36b39a6df0bc73dd180c993a57448b5dd1f807a8
cezar alejandro ramirez <genomma@live.com>
2015-12-08 13:27:02 -0600

    fixed_dates_siteviews_views

--- db/migrate/20150811155602_create_system_siteviews.rb
+++ db/migrate/20150811155602_create_system_siteviews.rb
@@ -3,7 +3,7 @@ class CreateSystemSiteviews < ActiveRecord::Migration
     create_table :system_siteviews do |t|
       t.integer :renewal_id, null: false
       t.datetime :visit_date, null: false
-	  t.string :google_event_start, null: false
+	    t.string :google_event_start, null: false
       t.boolean :completed
       t.string :delayed_id_start
       
