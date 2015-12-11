4229a52b76de1ae453119b9212aeaf96c2c8798f
Daniela Cerrillo <nanish_20@hotmail.com>
2015-12-11 01:14:31 -0600

    Validations, forms, translate

--- app/models/catalogs/supplier.rb
+++ app/models/catalogs/supplier.rb
@@ -3,7 +3,8 @@ include Searchable
 
 has_many :Contracts
 validates :business_name, uniqueness: true, presence: true
-validates :contact, :phone, :email, :phone_office, presence: true
+validates :contact, :phone, :phone_office, presence: true
+validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, presence: true
 
 def short_name(length=18)
 	business_name.truncate(length)
