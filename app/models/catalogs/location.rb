58153643cd61935301d37bfff9bf9ffa820be913
Daniela Cerrillo <nanish_20@hotmail.com>
2015-12-11 01:47:29 -0600

    view_details, email_format

--- app/models/catalogs/location.rb
+++ app/models/catalogs/location.rb
@@ -1,6 +1,7 @@
 class Catalogs::Location < ActiveRecord::Base
 has_many :Devices
-validates :department, :responsible, :email, presence: true
+validates :department, :responsible, presence: true
+validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, presence: true
 
   def short_name(length=18)
 	department.truncate(length)
