4cb19f1c7c81b8e317f8339ded20da451572b285
cesar-alamilla <czar.5@hotmail.com>
2015-12-11 22:43:04 -0600

    Fixed configuration mailer
    Se cambio la configuracion del mailer para que tome el texto del archivo
    de configuracion

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -23,33 +23,41 @@ class ApplicationMailer < ActionMailer::Base
 		#@user = user
 		#@url  = 'http://codeHero.co'
 		#email.each do |correo|
+    file_yaml = YAML.load_file "#{Rails.root}/config/config.yml"
+    @before_days = file_yaml["production"]['notification_time'].to_i.days
+
     case notification_type
       when "create_contract"
-      
       @header = "Este es un correo de notificación del contrato " + @system_contract.contract_no.to_s + " con el INB de la UNAM" 
       @contenido = "La descripción del contrato es la siguiente " + @system_contract.description.to_s + "con fecha de inicio " + @start_date.to_s + " y con fecha final " + @end_date.to_s
+      @config_text_contract = file_yaml["production"]['e_mail_create_contract'].to_s
       mail(to: @email, subject: 'Creacion de contracto con el INB')
     when "update_contract"
       @header = "Este es un correo de notificación del contrato " + @system_contract.contract_no + " con el INB de la UNAM" 
       @contenido = "La descripción del contrato es la siguiente " + @system_contract.description
+      @config_text_contract = file_yaml["production"]['e_mail_update_contract'].to_s
       mail(to: @email, subject: 'Actualizacion de contracto con el INB')
     when "create_renewal"
       @header = "Este es un correo de renovacion del contrato " + @system_contract.contract_no + " con el INB de la UNAM" 
       @contenido = "La descripción del contrato es la siguiente " + @system_contract.description + "con fecha de inicio " + @start_date.to_s + " y fecha de finalización de " + @end_date.to_s
+      @config_text_contract = file_yaml["production"]['e_mail_create_renewal'].to_s
       mail(to: @email, subject: 'Creacion de renovacion con el INB')
     when "update_renewal"
       @header = "Este es un correo de renovacion del contrato " + @system_contract.contract_no + " con el INB de la UNAM" 
       @contenido = "La descripción del contrato es la siguiente " + @system_contract.description + "con fecha de inicio " + @start_date.to_s + " y fecha de finalizacion de " + @end_date.to_s
+      @config_text_contract = file_yaml["production"]['e_mail_update_renewal'].to_s
       mail(to: @email, subject: 'Actualizacion de renovacion con el INB')
     when "create_siteview"
       @header = "Este es un correo de notificación de visita por el contrato " + @system_contract.contract_no + " con el INB de la UNAM" 
       @contenido = "La descripción del contrato es la siguiente " + @system_contract.description + "Fecha de visita: " + @start_date.to_s 
+      @config_text_contract = file_yaml["production"]['e_mail_create_siteview'].to_s
       mail(to: @email, subject: 'Creacion de visita con el INB')
     when "update_siteview"
       @header = "Este es un correo de actualizacion de visita por el contrato " + @system_contract.contract_no + " con el INB de la UNAM" 
       @contenido = "La descripción del contrato es la siguiente " + @system_contract.description + "Fecha de visita:" + @start_date.to_s
-	    mail(to: @email, subject: 'Actualizacion de visita con el INB')
-    end 
+      @config_text_contract = file_yaml["production"]['e_mail_update_siteview'].to_s
+      mail(to: @email, subject: 'Actualizacion de visita con el INB')
+    end
 	end
 
 end
